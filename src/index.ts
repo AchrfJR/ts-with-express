d25495a8ed8d1b4535dbf1b3d3a5ba3d90bdc050
fortestjr <malekkmalekk4444@gmail.com>
2024-10-27 21:57:06 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // Added the extended option
+
+
+const PORT  = process.env.PORT || 3333
+
+app.listen(PORT , () =>{
+    console.log(`Server Is Running On ${PORT}`)
+})
+
