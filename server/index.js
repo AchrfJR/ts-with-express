f05a79a410293d990d7a6e9c82336b2b00fa7fe2
fortestjr <malekkmalekk4444@gmail.com>
2024-10-28 15:38:38 +0300

    init sqLite Database with migrations

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { open, } from 'sqlite';
+import sqlite3 from 'sqlite3';
+import path from 'path';
 dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // Added the extended option
-const PORT = process.env.PORT || 3333;
-app.listen(PORT, () => {
-    console.log(`Server Is Running On ${PORT}`);
-});
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+(async () => {
+    const db = await open({
+        filename: path.join(__dirname, "sqLiteDb.sqlite"),
+        driver: sqlite3.Database
+    });
+    await db.migrate({
+        migrationsPath: path.join(__dirname, "migrations")
+    });
+    const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true })); // Added the extended option
+    const PORT = process.env.PORT || 3333;
+    app.listen(PORT, () => {
+        console.log(`Server Is Running On ${PORT}😂`);
+    });
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+import { open , } from 'sqlite';
+import sqlite3  from 'sqlite3';
+import path from 'path';
+dotenv.config();
+import { fileURLToPath } from 'url';
 
-dotenv.config()
-const app = express()
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+(
+    async()=>{
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true })) // Added the extended option
+    const db = await open({
+        filename : path.join(__dirname , "sqLiteDb.sqlite"), 
+        driver:sqlite3.Database
+    }) 
+    await db.migrate({
+        migrationsPath : path.join(__dirname , "migrations")
+    })
+    const app = express()
+
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true })) // Added the extended option
 
 
-const PORT  = process.env.PORT || 3333
+    const PORT  = process.env.PORT || 3333
 
-app.listen(PORT , () =>{
-    console.log(`Server Is Running On ${PORT}`)
-})
+    app.listen(PORT , () =>{
+        console.log(`Server Is Running On ${PORT}😂`)
+    })
 
+})()
